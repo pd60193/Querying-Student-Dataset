655a83cc1c3a7b26a40d8d07b63c4a339a16e85c
Priyam Dhanuka <priyamdhanuka@Priyams-MacBook-Pro.local>
2026-01-05 15:54:08 -0800

    Updating run script

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 cd client
 ng build --configuration production
 cd ..
-cp -R /Users/priyamdhanuka/raindrop-express-app/client/dist/client/browser/* ./public/ 
+rmdir ./public
+mkdir ./public
+cp -R ./client/dist/client/browser/* ./public/ 
 node app.js
