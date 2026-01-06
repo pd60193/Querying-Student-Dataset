717efa24252d495502d879f976f6f8b2742fb5a9
Priyam Dhanuka <priyamdhanuka@Priyams-MacBook-Pro.local>
2026-01-05 16:03:39 -0800

    Updating run script again

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
+rm -rf ./node_modules
+npm install
 npm install -g @angular/cli
 cd client
 ng build --configuration production
 cd ..
-rmdir -rf ./public
+rm -rf ./public
 mkdir ./public
 cp -R ./client/dist/client/browser/* ./public/ 
 node app.js
